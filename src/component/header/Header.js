412b8e45e009f1ca640358de9fc70f6687883368
sween <khansween@gmail.com>
2024-02-13 23:58:30 +0530

    written logic for search input

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import SearchIcon from '@mui/icons-material/Search';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import CloseIcon from '@mui/icons-material/Close';
 import "./Header.css";
+
 
 function Header() {
 
-  function showDropDown(){
-
-  }
+  const [searchInput, setSearchInput] = useState("");
+  const [showDropDown, setShowDropDown] = useState(false);
+  const [closeiconShow, setCloseiconShow] = useState(false)
+    
+ 
+  useEffect(() => {
+    if(searchInput === ""){
+      setShowDropDown(true) 
+      setCloseiconShow(false)
+    }else{
+      setShowDropDown(false)
+      setCloseiconShow(true)
+    }
+  },[searchInput])
+    
+  
+ 
 
   return (
     <header>
@@ -16,16 +34,26 @@ function Header() {
         </div>
         <div className="search-container">
           <div className="search-container-inner">
-            <div className="search-icon-container"></div>
-            <input type="text" placeholder="Try Jeans, Shirt, Saree, Kurti" onClick={showDropDown}/>
+            <div className="search-icon-container">
+              <SearchIcon sx={{ fontSize: 32}} color="disabled" />
+            </div>
+            <input type="text" onChange={(e)=>setSearchInput(e.target.value)} 
+            value={searchInput} 
+            onMouseOver={() => setShowDropDown(true)}
+            placeholder="Try Jeans, Shirt, Saree, Kurti" />
+            <div onClick={()=> setSearchInput("")} className={`close-icon ${closeiconShow ? "show" : ""}`} ><CloseIcon sx={{ fontSize:28}} /></div>
           </div>
-          <div className="search-container-dropDown">
+          <div className={`search-container-dropDown ${showDropDown ? "show" : ""}`} >
              <p>Kurti</p>
              <p>Saree</p>
              <p>Jeans</p>
              <p>Shirt</p>
              <p>T-Shirt</p>
              <p>Hoddy</p>
+             <p>Jacket</p>
+             <p>Shoes</p>
+             <p>Top</p>
+             <p>Suit</p>
           </div>
         </div>
         <div className="nav-container">
@@ -42,7 +70,8 @@ function Header() {
               <span>Newsroom</span>
             </div>
             <div className="container-for-left-border"></div>
-            <div className="header-right-cont">
+            <div className="header-right-cont profile">
+              <span><AccountCircleIcon /></span>
               <span>Profile</span>
             </div>
           </div>
