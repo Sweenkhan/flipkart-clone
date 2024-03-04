0300cd1f3fde7bca189a4b797ab430fc0d6d99dd
sween <khansween@gmail.com>
2024-03-04 18:25:15 +0530

    added media query and second section content

--- src/component/home/homeSecondSection/HomeSecondSection.js
+++ src/component/home/homeSecondSection/HomeSecondSection.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import "./HomeSecondSection.css";
+import mensStoreImg from "../../images/mens-store.webp"
+import kidssStoreImg from "../../images/kids-store.webp"
+import womenStoreImg from "../../images/women-store.webp"
+
 
 function HomeSecondSection() {
   return (
     <div className='home-secondSection'> 
-     <div className=''>
-      <div>
-         
+      <h2>Be fashion forward</h2>
+     <div className='be-fashion-forwardStore'>
+      <div className='fashion-women-store'>
+          <img src={womenStoreImg} alt='women-store' />
       </div>
-      <div>
-
+      <div className='fashion-mens-store'>
+       <div className='mens-store'>
+         <img src={mensStoreImg} alt='mens-store'/>
+       </div>
+       <div className='kids-store'>
+         <img src={kidssStoreImg} alt='kids-store'/>
+       </div>
       </div>
      </div>
      
