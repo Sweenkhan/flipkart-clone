ac65ca21739a090dfc0d28dae1862ab1a0beda06
sween <khansween@gmail.com>
2024-04-01 15:14:42 +0530

    design for product card

--- src/component/home/homeProductSection/HomeProductSection.js
+++ src/component/home/homeProductSection/HomeProductSection.js
@@ -1,22 +1,47 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from "axios"
-
+import "./HomeProductSection.css"
 
 function HomeProductSection() {
+
+const [products, setProducts] = useState([])
+let discount = [10, 20, 30, 40]
 
 useEffect(() => {
     console.log("jii")
   axios.get("http://localhost:8000/allProducts")
   .then((result) => {
-       console.log(result.data)
+        if(result.data.status === 200){
+          setProducts(result.data.products)
+        }
   }).catch((err) => {
        console.log(err)
   })
 },[])
 
   return (
-    <div>
-      this is product page
+    <div className='cart'>
+       <div className='cartContainer'>
+
+         {(products.length>0) && products.map((product, index) => {
+          return (
+            <div key={index} className='productDiv'>
+              <div className='productImgCnt'>
+                <picture>
+                  <img src={product.image} alt='productImg'/>
+                </picture>
+              </div>
+              <div className='productDetail'>
+                <p>{(product.title.length > 20) ? `${product.title.substring(0, 20)}...`: product.title}</p>
+                 
+                <h3>${product.price}<span className='discountSpan'>{`  ${discount[Math.floor(Math.random() * discount.length)]}% off`}</span></h3>
+              </div>
+            </div>
+          )
+         })
+
+         }
+       </div>
     </div>
   )
 }
